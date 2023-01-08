1f460470d1d3138c41a5c415c3e5a77725a2c9c9
dr0neform <122148528+dr0neform@users.noreply.github.com>
2023-01-08 17:17:18 +0200

    Update load_header.py

--- scripts/load_header.py
+++ scripts/load_header.py
@@ -1,20 +1,20 @@
 import csv
-import datetime
+from datetime import datetime
 import os
 from proovitoo.models import Header
 
 
 def run():
-    with open(os.path.join('scripts', 'Header_data.csv')) as file:
+    with open(os.path.join('scripts','Header_data.csv')) as file:
         reader = csv.reader(file, delimiter=';')
         next(reader)  # Advance past the header
 
         Header.objects.all().delete()
 
         for row in reader:
-            header = Header(reg_code=str(row[0]),
-                            create_date=datetime.date.today(),
-                            name=str(row[1]),
-                            search_string='',
-                            autofill_string=str(row[0]) + ' - ' + str(row[1]))
+            header = Header(reg_code=str(row[1]),
+                            create_date=datetime.strptime(row[2],'%d.%m.%Y').date(),
+                            name=str(row[0]),
+                            search_string=row[3],
+                            autofill_string=str(row[1]) + ' - ' + str(row[0]))
             header.save()
